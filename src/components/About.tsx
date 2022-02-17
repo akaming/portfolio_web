f19537bc718be0e9138fe053c7edb245d98566c3
Lee myungmin <audals2478@gmail.com>
2022-02-17 17:35:26 +0900

    about content 추가

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,45 @@ import styled from "styled-components"
 const About = (props: any) => {
     return (
         <Container id={props.id}>
-            about
+            <Img src="/images/me.jpg"/>
+            <InfoTable>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <td>이명민</td>
+                    </tr>
+                    <tr>
+                        <th>Birth</th>
+                        <td>95.04.03</td>
+                    </tr>
+                    <tr>
+                        <th>Skill</th>
+                        <td>
+                            <ul>
+                                <li>HTML5</li>
+                                <li>CSS</li>
+                                <li>SCSS</li>
+                                <li>JavaScript</li>
+                                <li>React.js</li>
+                                <li>Next.js</li>
+                                <li>TypeScript</li>
+                                <li>Adonis.js</li>
+                                <li>Figma</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Company</th>
+                        <td>
+                            <ul>
+                                <li>2020.10 ~ </li>
+                                <li>2019.08 ~ 2020.08 에스와이소프트</li>
+                                <li>2016.11 ~ 2018.04 그룹아이디디</li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </InfoTable>
         </Container>
     )
 }
@@ -11,5 +49,7 @@ const About = (props: any) => {
 const Container = styled.div`
     min-height: 500px;
 `
+const Img = styled.img``
+const InfoTable = styled.table``
 
 export default About
