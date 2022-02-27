ca3f0c510b2ff55d69d865a6f8956c4800e3155f
Lee myungmin <audals2478@gmail.com>
2022-02-27 17:51:59 +0900

    about 내용 추가 및 reset 내용 수정

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,53 +3,66 @@ import styled from "styled-components"
 const About = (props: any) => {
     return (
         <Container id={props.id}>
-            <Img src="/images/me.jpg"/>
-            <InfoTable>
-                <tbody>
-                    <tr>
-                        <th>Name</th>
-                        <td>이명민</td>
-                    </tr>
-                    <tr>
-                        <th>Birth</th>
-                        <td>95.04.03</td>
-                    </tr>
-                    <tr>
-                        <th>Skill</th>
-                        <td>
-                            <ul>
-                                <li>HTML5</li>
-                                <li>CSS</li>
-                                <li>SCSS</li>
-                                <li>JavaScript</li>
-                                <li>React.js</li>
-                                <li>Next.js</li>
-                                <li>TypeScript</li>
-                                <li>Adonis.js</li>
-                                <li>Figma</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Company</th>
-                        <td>
-                            <ul>
-                                <li>2020.10 ~ </li>
-                                <li>2019.08 ~ 2020.08 에스와이소프트</li>
-                                <li>2016.11 ~ 2018.04 그룹아이디디</li>
-                            </ul>
-                        </td>
-                    </tr>
-                </tbody>
-            </InfoTable>
+            <ProfileImg />
+            <ProfileInfo>
+                <dt>Name</dt>
+                <dd>이명민</dd>
+
+                <dt>Birth</dt>
+                <dd>95.04.03</dd>
+
+                <dt>Skill</dt>
+                <dd>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS</li>
+                        <li>SCSS</li>
+                        <li>JavaScript</li>
+                        <li>React.js</li>
+                        <li>Next.js</li>
+                        <li>TypeScript</li>
+                        <li>Adonis.js</li>
+                        <li>Figma</li>
+                    </ul>
+                </dd>
+     
+                <dt>Company</dt>
+                <dd>
+                    <ul>
+                        <li>2020.10 ~ </li>
+                        <li>2019.08 ~ 2020.08 에스와이소프트</li>
+                        <li>2016.11 ~ 2018.04 그룹아이디디</li>
+                    </ul>
+                </dd>
+            </ProfileInfo>
         </Container>
     )
 }
 
 const Container = styled.div`
-    min-height: 500px;
+    display: flex;
+    justify-content: space-around;
 `
-const Img = styled.img``
-const InfoTable = styled.table``
+
+const ProfileImg = styled.div`
+    width: 283px;
+    height: 400px;
+    background: url("/images/me.jpg") no-repeat;
+    transition: background .5s;
+
+    &:hover {
+        background: url("/images/me_hover.jpg") no-repeat;
+    }
+`
+
+const ProfileInfo = styled.dl`
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    row-gap: 10px;
+
+    dt {
+        font-weight: bold;
+    }
+`
 
 export default About
