8b8291874df933542a7d64942e5aa1819cdcd19c
Lee myungmin <audals2478@gmail.com>
2022-03-17 18:13:46 +0900

    intro id 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,20 @@ import About from '../src/components/About'
 import Contact from '../src/components/Contact'
 import { Container } from '../src/components/Container'
 import Header from '../src/components/Header'
+import Intro from '../src/components/Intro'
 import Works from '../src/components/Works'
 import { parseCookies } from "../src/helpers/"
 
 export default function Home() {
   return (
-    <Container>
+    <>
       <Header />
-      <About id="about"/>
-      <Works id="works"/>
-      <Contact id="contact"/>
-    </Container>
+      <Intro id="intro"/>
+      <Container>
+        <About id="about"/>
+        <Works id="works"/>
+        <Contact id="contact"/>
+      </Container>
+    </>
   )
 }
